3ca828069974ab142d56a71f24e61d6bf3eea295
Michael Burden <maburdenjr@gmail.com>
2014-09-07 02:08:27 -0700

    Codebase Sync

--- ikiosk/webapps/ikioskmcp/ajaxHandler.php
+++ ikiosk/webapps/ikioskmcp/ajaxHandler.php
@@ -33,11 +33,11 @@ if (isset($_GET['ajaxAction'])) {
 				if (!copy($sourceFile, $destinationFile)) {
 					errorLog("Unable to copy ".$sourceFile." to ".$destinationFile, "System Error", $redirect);
 				}
-				$productionList .= "<tr><td class=\"truncate\">".$sourceFile."</td><td>".$destinationFile."</td></tr>";
+				$productionList .= "<tr><td class=\"truncate-list\">".$sourceFile."</td><td class=\"truncate-list\">".$destinationFile."</td></tr>";
 				if (!copy($sourceFile, $destinationFile)) {
 					errorLog("Unable to copy ".$sourceFile." to ".$packageFile, "System Error", $redirect);
 				}
-				$packageList .= "<tr><td class=\"truncate\">".$sourceFile."</td><td>".$packageFile."</td></tr>";
+				$packageList .= "<tr><td class=\"truncate-list\">".$sourceFile."</td><td class=\"truncate-list\">".$packageFile."</td></tr>";
 			}
 			
 			$response .= $productionList.$packageList;
