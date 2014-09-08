8dff6444775d51392c4ee31c67b7dd9956a731b9
Michael Burden <maburdenjr@gmail.com>
2014-09-07 21:00:17 -0700

    Modal Updates

--- ikiosk/webapps/cms/index.php
+++ ikiosk/webapps/cms/index.php
@@ -3,3 +3,12 @@
 $PAGE['application_code'] = "CMS";
 require('../../includes/core/ikiosk.php');
 ?>
+<div class="row">
+  <div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
+    <h1 class="page-title">iKioskCMS</h1>
+  </div>
+</div>
+<section id="widget-grid"> </section>
+<script type="text/javascript">
+		pageSetUp();
+</script> 
