e17d7b8d729225f509be411a0d35b347bf1fd075
Michael Burden <maburdenjr@gmail.com>
2014-09-07 20:57:46 -0700

    Modal Updates

--- ikiosk/webapps/admin/index.php
+++ ikiosk/webapps/admin/index.php
@@ -3,3 +3,12 @@
 $PAGE['application_code'] = "SYS";
 require('../../includes/core/ikiosk.php');
 ?>
+<div class="row">
+  <div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
+    <h1 class="page-title">System Administration</h1>
+  </div>
+</div>
+<section id="widget-grid"> </section>
+<script type="text/javascript">
+		pageSetUp();
+</script> 
---
+++ ikiosk/webapps/ikioskmcp/index.php
@@ -0,0 +1,14 @@
+<?php
+/* iKiosk 7.0 Tiger */
+$PAGE['application_code'] = "SYS";
+require('../../includes/core/ikiosk.php');
+?>
+<div class="row">
+  <div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
+    <h1 class="page-title">iKiosk Master Control Panel</h1>
+  </div>
+</div>
+<section id="widget-grid"> </section>
+<script type="text/javascript">
+		pageSetUp();
+</script> 
