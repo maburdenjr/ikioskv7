4bd6f008f27f97ac69a60896ef4304ed9f5f81de
Mike Burden <michaelb@loyal3.com>
2014-09-10 16:50:45 -0700

    CMS FrontEnd

---
+++ ikiosk/webapps/cms/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+/* iKiosk 7.0 Tiger */
+$PAGE['application_code'] = "CMS";
+require('../../includes/core/ikiosk.php');
+?>
+<div class="row">
+  <div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
+    <h1 class="page-title">iKioskCMS</h1>
+  </div>
+</div>
+<section id="widget-grid"> </section>
+<script type="text/javascript">
+		pageSetUp();
+</script> 
---
+++ ikiosk/webapps/cms/login.php
@@ -0,0 +1,4 @@
+<?php
+/* iKiosk 7.0 Tiger */
+$SITE['site_id'] = "ikiosk_tmp_site";
+?>
