9c266e9af7246f723f9485e4d8662510d762e422
Michael Burden <maburdenjr@gmail.com>
2014-09-11 21:00:40 -0700

    Page Properties

---
+++ ikiosk/webapps/cms/admin-pageProperties.php
@@ -0,0 +1,5 @@
+<form id = "cms-editPageProperties" class="smart-form">
+	<div class="modal-header">
+  	<h4 class="modal-title"><i class="fa fa-wrench"></i> Edit Page Properties</h4>
+  </div>
+</form>
