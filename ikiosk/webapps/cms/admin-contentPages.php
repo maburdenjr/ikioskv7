32e765903138b0087c6287c0da9644c4b7e6cf2a
Mike Burden <michaelb@loyal3.com>
2014-09-13 14:13:14 -0700

    Modal Menu

---
+++ ikiosk/webapps/cms/admin-contentPages.php
@@ -0,0 +1,7 @@
+<div class="modal-header">
+  <h4 class="modal-title">Content Pages</h4>
+</div>
+<div class="modal-body"> </div>
+<div class="modal-footer">
+  <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close </button>
+</div>
