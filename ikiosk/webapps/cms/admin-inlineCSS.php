5332b2b44a27abd1cda4950d465594b20a9acd96
Mike Burden <michaelb@loyal3.com>
2014-09-19 16:56:44 -0700

    Style Editor

--- ikiosk/webapps/cms/admin-inlineCSS.php
+++ ikiosk/webapps/cms/admin-inlineCSS.php
@@ -1,3 +1,27 @@
 <div class="widget-popover-title">CSS Style Editor</div>
-<div class="widget-popover-body custom-scroll">
+<div class="widget-popover-body">
+  <ul id="styleMenu" class="nav nav-tabs">
+    <li class="dropdown"> <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">Style Category <b class="caret"></b></a>
+      <ul class="dropdown-menu">
+        <li><a href="#s1" data-toggle="tab">Font</a></li>
+        <li><a href="#s2" data-toggle="tab">Size & Position</a></li>
+        <li><a href="#s4" data-toggle="tab">Background</a></li>
+        <li><a href="#s5" data-toggle="tab">Margins & Padding</a></li>
+        <li><a href="#s7" data-toggle="tab">Borders</a></li>
+        <li><a href="#s9" data-toggle="tab">List</a></li>
+        <li><a href="#s10" data-toggle="tab">Other</a></li>
+        <li><a href="#s11" data-toggle="tab">Attributes</a></li>
+      </ul>
+    </li>
+  </ul>
+  <div id="styleCategories" class="tab-content custom-scroll">
+    <div id="s1" class="tab-pane fade in active">Hi </div>
+    <div id="s2" class="tab-pane fade in ">Hi </div>
+    <div id="s4" class="tab-pane fade in ">Hi </div>
+    <div id="s5" class="tab-pane fade in ">Hi </div>
+    <div id="s7" class="tab-pane fade in ">Hi </div>
+    <div id="s9" class="tab-pane fade in ">Hi </div>
+    <div id="s10" class="tab-pane fade in ">Hi </div>
+    <div id="s11" class="tab-pane fade in ">Hi </div>
+  </div>
 </div>
