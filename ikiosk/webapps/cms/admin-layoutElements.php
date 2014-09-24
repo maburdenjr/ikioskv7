73ee6a47578c7f909ac9b49255bbb3f3809ad928
Mike Burden <michaelb@loyal3.com>
2014-09-23 17:17:10 -0700

    Drag and Drop Elements

---
+++ ikiosk/webapps/cms/admin-layoutElements.php
@@ -0,0 +1,18 @@
+<?php
+$col2 = '<div class="row"><div class="col-md-6"></div><div class="col-md-6"></div></row>';
+$col3 = '<div class="row"><div class="col-md-4"></div><div class="col-md-4"></div><div class="col-md-4"></div>
+</row>';
+$col4 = '<div class="row"><div class="col-md-3"></div><div class="col-md-3"></div><div class="col-md-3"></div><div class="col-md-3"></div></row>';
+$col6 = '<div class="row "><div class="col-md-2"></div><div class="col-md-2"></div><div class="col-md-2"></div><div class="col-md-2"></div><div class="col-md-2"></div><div class="col-md-2"></div</row>';
+$col12 = '<div class="row"><div class="col-md-1"></div><div class="col-md-1"></div><div class="col-md-1"></div><div class="col-md-1"></div><div class="col-md-1"></div><div class="col-md-1"></div><div class="col-md-1"></div><div class="col-md-1"></div><div class="col-md-1"></div><div class="col-md-1"></div><div class="col-md-1"></div><div class="col-md-1"></div></row>';
+?>
+
+<div id="layoutElements">
+  <div class="acc-section-trigger"><i class="fa fa-fw fa-columns"></i> Multi-Column Layouts</div>
+  <div class="acc-section-content custom-scroll">
+  	<div class="layoutItem ui-element" data-code="<?php echo htmlentities($col2); ?>">
+    	<div class="layoutImage"></div>
+    	<div class="layoutTitle">2 Columns</div>	
+    </div>
+  </div>
+</div>
