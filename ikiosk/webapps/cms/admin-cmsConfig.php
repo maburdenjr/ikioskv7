31b1e9b43de91a9dbdd80b9f8264cc66e4fd325b
Michael Burden <maburdenjr@gmail.com>
2014-09-28 23:08:21 -0700

    CMS Enhancements

--- ikiosk/webapps/cms/admin-cmsConfig.php
+++ ikiosk/webapps/cms/admin-cmsConfig.php
@@ -8,7 +8,7 @@
       <li><a href="/cms/ajaxHandler.php?ajaxAction=fileManager&appCode=CMS" class="modalDynLink"><i class="fa fa-folder"></i> File Manager</a></li>
       <li><a href="/cms/ajaxHandler.php?ajaxAction=photoGallery&appCode=CMS" class="modalDynLink"><i class="fa fa-file-image-o"></i> Photos</a></li>
       <li><a href="/cms/ajaxHandler.php?ajaxAction=cmsSettings&appCode=CMS" class="modalDynLink"><i class="fa fa-cog"></i> Settings</a></li>
-      <li><a href="/cms/ajaxHandler.php?ajaxAction=templates&appCode=CMS" class="modalDynLink"><i class="fa fa-cube"></i> Templates</a></a></li>
+      <li><a href="/cms/ajaxHandler.php?ajaxAction=templates&appCode=CMS" class="modalDynLink"><i class="fa fa-cube"></i> Templates</a></li>
     </ul>
   </div>
   <div id ="dynModalContent">
@@ -16,7 +16,19 @@
       <h4 class="modal-title">iKioskCMS</h4>
     </div>
     <div class="modal-body">
-    
+    	<div class="row cmsIcons">
+      	<div class="col-md-2"><a href="/cms/ajaxHandler.php?ajaxAction=blogArticles&appCode=CMS" class="modalDynLink btn btn-default"><i class="fa fa-file-text-o fa-3x"></i> Blog Articles</a></div>
+        <div class="col-md-2"><a href="/cms/ajaxHandler.php?ajaxAction=contentPages&appCode=CMS" class="modalDynLink btn btn-default"><i class="fa fa-file-text-o fa-3x"></i> Content Pages</a></div>
+        <div class="col-md-2"><a href="/cms/ajaxHandler.php?ajaxAction=codeSnippets&appCode=CMS" class="modalDynLink btn btn-default"><i class="fa fa-file-code-o fa-3x"></i> Code Snippets</a></div>
+        <div class="col-md-2"><a href="/cms/ajaxHandler.php?ajaxAction=fileManager&appCode=CMS" class="modalDynLink btn btn-default"><i class="fa fa-folder fa-3x"></i> File Manager</a></div>
+        <div class="col-md-2"><a href="/cms/ajaxHandler.php?ajaxAction=photoGallery&appCode=CMS" class="modalDynLink btn btn-default"><i class="fa fa-file-image-o fa-3x"></i> Photos</a></div>
+        <div class="col-md-2"><a href="/cms/ajaxHandler.php?ajaxAction=cmsSettings&appCode=CMS" class="modalDynLink btn btn-default"><i class="fa fa-cog fa-3x"></i> Settings</a></div>
+      </div>
+      <div class="row cmsIcons">
+      	<div class="col-md-2"><a href="/cms/ajaxHandler.php?ajaxAction=templates&appCode=CMS" class="modalDynLink btn btn-default"><i class="fa fa-cube fa-3x"></i> Templates</a></div>
+        
+      </div>
+      
     </div>
     <div class="modal-footer">
       <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close </button>
