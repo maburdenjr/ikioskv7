ad66a2604478c2e69f211bed20abf53dea610352
Mike Burden <michaelb@loyal3.com>
2014-09-30 17:08:11 -0700

    Modifying Layout Files

--- ikiosk/webapps/cms/admin-editor.php
+++ ikiosk/webapps/cms/admin-editor.php
@@ -11,11 +11,13 @@
   <input type="hidden" name="iKioskForm" value="Yes" />
   <input type="hidden" name="appCode" value="CMS" />
  </form>
- 
+ <?php
+ if (is_file($SYSTEM['ikiosk_filesystem_root']."/sites".$SITE['site_root']."/templates/".$row_getTemplate['template_id']."-layouts.php")) { ?>
    <div id="layoutWidget" class="cms-widget">
     <p class="cms-widget-title">Layout Elements<span style="font-size: 11px; line-height:100%; display:block; margin-top:5px; color: #CCC;">Add elements to your page by dragging them onto the canvas.</span></p>
     
     <?php include($SYSTEM['ikiosk_filesystem_root']."/ikiosk/webapps/cms/admin-layoutElements.php");
   ?>
   </div>
+  <?php } ?>
 </div>
